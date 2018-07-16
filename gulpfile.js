895c8174884de37dcab8074c457c603699165126
Mike Key <mike@mk3y.com>
2018-07-15 18:45:38 -0600

    updates to my workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,51 @@
 // Mike's Gulp Starter Workflow
 'use strict';
 
+// TO DO LIST: 
+// 1.) Add production build function & seperate default
+// 2.) JS Import doesn't work - can't go up a directory, need a replacement
+// 3.) Imagemin - says it compressed images, isn't sending anything to dest folder??
+
 // Require our stuff
 var    gulp = require('gulp'),
 browserSync = require('browser-sync').create(),
+   partials = require('gulp-inject-partials'),
    cleanCSS = require('gulp-clean-css'),
+   jsImport = require('gulp-js-import'),
+   imagemin = require('gulp-imagemin'),
+    htmlmin = require('gulp-htmlmin'),
      concat = require('gulp-concat'),
      uglify = require('gulp-uglify'),
      rename = require('gulp-rename'),
+      gutil = require('gulp-util'),
        sass = require('gulp-sass'),
        maps = require('gulp-sourcemaps');
 
 
 // Set those paths
 const base_path = './',
-            src = base_path + 'src',
+            src = base_path + '_src',
            dist = base_path + 'build',
           paths = {
               js: src + '/js/*.js',
-            scss: src + '/sass/styles.scss'
+             img: src + '/img/*',
+            html: src + '/html/*.html',
+            scss: src + '/sass/app.scss'
           };
+
+
+// Do stuff with HTML
+gulp.task('makeHTML', function () {
+    return gulp.src(paths.html)
+        .pipe(partials({
+            start: '<% {{path}} ',
+              end: '%>',
+           removeTags: true
+           }))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(dist))
+        .pipe(browserSync.stream());
+});
 
 
 // Do stuff with SASS
@@ -39,8 +65,11 @@ gulp.task('makeCSS', () => {
 gulp.task('makeJS', () => {
     return gulp.src(paths.js)
         .pipe(maps.init())
+        .pipe(jsImport({hideConsole: false}))
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(concat('scripts.js'))
         .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(rename('scripts.min.js'))
         .pipe(maps.write('./'))
         .pipe(gulp.dest(dist + '/assets'))
@@ -48,24 +77,41 @@ gulp.task('makeJS', () => {
 });
 
 
+// Let's compress some images
+gulp.task('compressIMG', () => {
+    return gulp.src(paths.img)
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        pipe(gulp.dest(dist + '/img'));
+});
+
+
 // Watch for changes and do stuff
 gulp.task('watch' , () => {
-    gulp.watch(src + '/sass/*.scss', ['makeCSS']);
+    gulp.watch(paths.scss, ['makeCSS']);
     gulp.watch(paths.js, ['makeJS']);
+    gulp.watch('./build/*.html').on('change', browserSync.reload);
 });
 
 
 // Setup a Gulp Server with Browser Sync
-gulp.task('serve', ['makeCSS', 'makeJS'], function() {
+gulp.task('serve', ['makeHTML', 'makeCSS', 'makeJS'], function() {
     browserSync.init({
-        server: {
-            baseDir: dist,
-            directory: true
-        }
+        server: './build/'
     });
     gulp.watch(src + '/sass/*.scss', ['makeCSS']);
     gulp.watch(paths.js, ['makeJS']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch(paths.html, ['makeHTML']);
+    gulp.watch(src + '/html/inc/*.html', ['makeHTML']); // pay attention to inc folder too
 });
 
 
--- readme.md
+++ readme.md
@@ -1,25 +1,33 @@
-#Gulp Starter Workflow
+# Gulp Starter Workflow
 
-This is a basic, and ever evolving workflow for working with Gulp projects. I wanted something could add to most projects, weather it was a WordPress theme, Jeykll site build or static HTML site build.
+This is a basic, and ever evolving workflow for using Gulp with various web projects. I wanted something could add to most projects, weather it was a WordPress theme, Jeykll site build or static HTML site build. 
+
+Currently this is configured around HTML static builds.
 
 The primary goals of this experiment:
 
 1. Compile sass, add prefixes, and spit out a minified file.
 2. Concat JS and spit out minified file.
-3. Build (Deployment structure of site)
-4. Serve HTML and refresh the browser with changes using browserSync
-5. Deploy changes to Github or Amazon AWS
+3. Compile HTML partials, compress and spit out file.
+4. Build (Deployment structure of site)
+5. Serve HTML and refresh the browser with changes using browserSync
+6. Deploy changes to Github or Amazon AWS
 
-Future plans
+TO-DO List
 
-Need to do add html-minified and a way to structure html into the final build for the workflow.
+This still needs work, right now I’m working on an HTML templating project. Still working out a few bugs, and need to make this so I can copy paste to various projects and get going.
+
+Also, need to improve this README
+
 
 #### Install Dependencies
 ```bash
 npm install
 ```
 
-#### Run development tasks:
+#### Run tasks:
 ```
 gulp
 ```
+
+## Contributors welcome! Cheers.
